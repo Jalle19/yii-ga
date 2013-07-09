8aead42f2116d07f79c62606ec3cb3ab6f1feaa2
Sam Stenvall <neggelandia@gmail.com>
2013-07-09 14:30:13 +0300

    initial import

---
+++ src/yiiga/components/GoogleAnalytics.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * GoogleAnalytics file.
+ * @author Sam Stenvall <sam.stenvall@arcada.fi>
+ * @author Christoffer Niska <ChristofferNiska@gmail.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package googleanalytics.components
+ */
+
+namespace yiiga\components;
+use \Yii as Yii;
+
+/**
+ * Application component for handling Google Analytics integration.
+ */
+class GoogleAnalytics extends \CApplicationComponent
+{
+	/**
+	 * @var string google analytics account ID (tracking code).
+	 */
+	public $accountId;
+	/**
+	 * @var string currency for the e-commerce tracking.
+	 */
+	public $currency;
+	
+	/**
+	 * @var string the domain for GATC cookies
+	 */
+	public $cookieDomain;
+	
+	/**
+	 * @var GATransaction[] list of registered transactions.
+	 */
+	private $_transactions = array();
+
+	/**
+	 * Creates a new transaction object.
+	 * @return GATransaction
+	 */
+	public function createTransaction()
+	{
+		return new Transaction();
+	}
+
+	/**
+	 * Registers the google-analytics tracking code.
+	 * Call this method on every page that needs tracking.
+	 * All registered transactions will also be registered.
+	 */
+	public function registerTracking()
+	{
+		if (!isset($this->accountId))
+			return;
+
+		$headScript = <<<EOD
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '{$this->accountId}']);
+EOD;
+
+		if(isset($this->cookieDomain))
+			$headScript .= "\n_gaq.push(['_setDomainName', '{$this->cookieDomain}']);";
+			
+		$headScript .= "\n_gaq.push(['_trackPageview']);";
+		$bodyEndScript = $this->getTransactionsJS();
+		$bodyEndScript .= <<<EOD
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+EOD;
+
+		Yii::app()->clientScript->registerScript(__CLASS__.'#tracking-head', $headScript, \CClientScript::POS_HEAD);
+		Yii::app()->clientScript->registerScript(__CLASS__.'#tracking-body', $bodyEndScript, \CClientScript::POS_END);
+	}
+
+	/**
+	 * Returns the JavaScript for the registered transactions.
+	 * @return string the script.
+	 */
+	protected function getTransactionsJS()
+	{
+		ob_start();
+		foreach($this->_transactions as $transaction)
+		{
+			echo "_gaq.push({$transaction->toJS()});".PHP_EOL;
+			echo $transaction->getItemsJS();
+			echo "_gaq.push(['_trackTrans']);".PHP_EOL;
+		}
+		return ob_get_clean();
+	}
+
+	/**
+	 * Registers a transaction.
+	 * @param GATransaction $transaction
+	 */
+	public function addTransaction($transaction)
+	{
+		$this->_transactions[] = $transaction;
+	}
+}
---
+++ src/yiiga/components/Transaction.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * GATransaction file.
+ * @author Sam Stenvall <sam.stenvall@arcada.fi>
+ * @author Christoffer Niska <ChristofferNiska@gmail.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package googleanalytics.components
+ */
+
+namespace yiiga\components;
+
+/**
+ * Component that describes a Google Analytics transaction.
+ * Stores the transactions items as well as the general transaction properties such as orderID etc.
+ */
+class Transaction extends \CComponent
+{
+	/**
+	 * @var string order ID.
+	 */
+	public $orderId = '';
+	/**
+	 * @var string affiliation (usually the company name).
+	 */
+	public $affiliation = '';
+	/**
+	 * @var string total tax on the order.
+	 */
+	public $tax = '';
+	/**
+	 * @var float total shipping costs for the order.
+	 */
+	public $shipping = 0.00;
+	/**
+	 * @var string customer city.
+	 */
+	public $city = '';
+	/**
+	 * @var string customer state.
+	 */
+	public $state = '';
+	/**
+	 * @var string customer country.
+	 */
+	public $country = '';
+	/**
+	 * @var float the total value of the order.
+	 */
+	public $total = 0.00;
+	/**
+	 * @var GATransactionItem[] list of transaction items.
+	 */
+	protected $_items = array();
+	
+	/**
+	 * Creates a new transaction item object.
+	 * @return GATransactionItem the item.
+	 */
+	public function createItem() 
+	{
+		return new TransactionItem();
+	}
+	
+	/**
+	 * Adds an item to the transaction.
+	 * @param GATransactionItem $item the item.
+	 */
+	public function addItem($item) 
+	{
+		$item->orderId = $this->orderId;
+		$this->_items[] = $item;
+	}
+
+	/**
+	 * Returns the JavaScript for the items the transaction holds.
+	 * @return string the script.
+	 */
+	public function getItemsJS()
+	{
+		$script = '';
+
+		foreach ($this->_items as $item)
+			$script .= '_gaq.push('.$item->toJS().');'.PHP_EOL;
+
+		return $script;
+	}
+	
+	/**
+	 * Returns the current object definition as a JavaScript array (starting with _addTrans).
+	 * @return string the script.
+	 */
+	public function toJS()
+	{
+		$data = array('_addTrans');
+		foreach(array('orderId', 'affiliation', 'total', 'tax', 'shipping', 'city', 'state', 'country') as $name) 
+			$data[] = $this->{$name};
+		
+		return \CJavaScript::encode($data);
+	}
+}
